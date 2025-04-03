8b992104bad9e5a9c12da60d8ee26d9585912cb0
Maria Kesa <maria.kesa@gmail.com>
2025-04-03 22:44:19 +0300

    dat

---
+++ src/brain_area_embedder/brain_embedder.py
@@ -0,0 +1,13 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
+
+from dotenv import load_dotenv
+
+load_dotenv()
